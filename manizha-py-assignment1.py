03172bd9c49560f36803a01f8df668672b7e293a
Manizha <59837609+manizha01@users.noreply.github.com>
2020-04-29 09:46:12 -0400

    Update manizha-py-assignment1.py

--- manizha-py-assignment1.py
+++ manizha-py-assignment1.py
@@ -20,19 +20,17 @@ for number in course_mark:
     total = total + number
     #print(total)
 
-avg = 0
-for num in course_mark:
-    avg = total/num_of_courses
-result = input(f"Your average for your {num_of_courses} courses is:  {avg}")
+    average = total/num_of_courses
+result = input(f"Your average for your {num_of_courses} courses is:  {average}")
 #print("your average for your courses is:" + str(avg))
 
-if avg >= 90  and avg <= 100:
+if average >= 90  and average <= 100:
     print("your grade is A+")
-elif avg >= 80 and avg <=89:
+elif average >= 80 and average <=89:
     print("your grade is B")
-elif avg >= 70 and avg <=79:
+elif average >= 70 and average <=79:
     print("your grade is C")
-elif avg >= 60 and avg <= 69:
+elif average >= 60 and average <= 69:
     print("your grade is D")
 elif avg < 60:
     print("your grade is F")
